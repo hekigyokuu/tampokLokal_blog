fec10dc94ec2ae4f57c4be5faf8aaa699ee0c4cf
hekigyokuu <hekiigyokuu@gmail.com>
2025-03-28 21:12:11 +0800

    fix text output and blog schema

--- models/Blog.js
+++ models/Blog.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const blogSchema = new mongoose.Schema({
   username: { type: String, trim: true },
   title: { type: String, required: true, trim: true },
-  content: { type: String, required: true, trim: true },
+  content: { type: String, required: true },
 }, { timestamps: true });
 
 const Blog = mongoose.model('Blog', blogSchema);
